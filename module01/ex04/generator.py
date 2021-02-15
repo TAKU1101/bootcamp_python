34585d052bbe2ef507ef239c15519663222f5aec
Taku <you@example.com>
2021-02-15 12:04:55 +0900

    Add: generator mes

---
+++ module01/ex04/generator.py
@@ -0,0 +1,20 @@
+from random import randint
+from collections import Counter
+
+
+def generator(text, sep=" ", option=None):
+    '''Option is an optional arg, sep is mandatory'''
+    word_list = text.split(sep)
+    if option == "shuffle":
+        shuffle_list = []
+        size = len(word_list)
+        for i in range(size):
+            shuffle_list.append(word_list.pop(randint(0, len(word_list) - 1)))
+        word_list = shuffle_list
+    elif option == "ordered":
+        word_list.reverse()
+    elif option == "unique":
+        countDir = Counter(word_list)
+        word_list = countDir.keys()
+    for word in word_list:
+        yield word
---
+++ module01/ex04/test_gen.py
@@ -0,0 +1,23 @@
+from generator import generator
+
+if __name__ == '__main__':
+    print("============ shuffle option ============")
+    text = "Le Lorem Ipsum est simplement du faux texte."
+    for word in generator(text, option="shuffle"):
+        print(word)
+
+    print("============ ordered option ============")
+    text = "Le Lorem Ipsum est simplement du faux texte."
+    for word in generator(text, option="ordered"):
+        print(word)
+
+    print("============ unique option ============")
+    text = "Lorem Ipsum Lorem Ipsum"
+    for word in generator(text, option="unique"):
+        print(word)
+
+    print("============ no option ============")
+    text = "Le Lorem Ipsum est simplement du faux texte."
+    for word in generator(text):
+        print(word)
+
